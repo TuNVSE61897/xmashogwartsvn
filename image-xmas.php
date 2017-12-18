3c55c4ef9f64d8ffa07c3e3ee0f348dcac4d2bca
Tu Nguyen <tu@instaroid.sg>
2017-12-19 00:10:56 +0700

    change hair color

--- image-xmas.php
+++ image-xmas.php
@@ -2,39 +2,6 @@
 
 <?php
 
-function mergeImages() {
-//define the width and height of our images
-    define("WIDTH", 500);
-    define("HEIGHT", 500);
-    $dest_image = imagecreatetruecolor(WIDTH, HEIGHT);
-//make sure the transparency information is saved
-    imagesavealpha($dest_image, true);
-//create a fully transparent background (127 means fully transparent)
-    $trans_background = imagecolorallocatealpha($dest_image, 0, 0, 0, 127);
-//fill the image with a transparent background
-    imagefill($dest_image, 0, 0, $trans_background);
-//take create image resources out of the 3 pngs we want to merge into destination image
-    $a = imagecreatefrompng('images/1.png');
-    $b = imagecreatefrompng('images/2.png');
-    $c = imagecreatefrompng("images/3.png");
-
-//copy each png file on top of the destination (result) png
-    imagecopy($dest_image, $a, 0, 0, 0, 0, WIDTH, HEIGHT);
-    imagecopy($dest_image, $b, 0, 0, 0, 0, WIDTH, HEIGHT);
-    imagecopy($dest_image, $c, 0, 0, 0, 0, WIDTH, HEIGHT);
-
-//send the appropriate headers and output the image in the browser
-    header('Content-Type: image/png');
-
-    imagepng($dest_image, "images/test.png");
-//imagedestroy($im);
-//destroy all the image resources to free up memory
-    imagedestroy($a);
-    imagedestroy($b);
-    imagedestroy($c);
-    imagedestroy($dest_image);
-}
-
 function getOptions($shirtOption = 'option1', $hairOption = 'option1') {
     switch ($shirtOption) {
         case 'option1':
@@ -112,6 +79,14 @@ $hairPath = 'images/hair/' . $hair . '.png';
 $a = imagecreatefrompng('images/0.png');
 $b = imagecreatefrompng($shirtPath);
 $c = imagecreatefrompng($hairPath);
+
+//change hair color
+//$rgb = array(255-$rgb[0],255-$rgb[1],255-$rgb[2]);
+imagefilter($c, IMG_FILTER_NEGATE); 
+imagefilter($c, IMG_FILTER_COLORIZE, 255, 0, 55); 
+imagefilter($c, IMG_FILTER_NEGATE); 
+imagealphablending( $c, false );
+imagesavealpha( $c, true );
 
 //copy each png file on top of the destination (result) png
 imagecopy($dest_image, $a, 0, 0, 0, 0, WIDTH, HEIGHT);
